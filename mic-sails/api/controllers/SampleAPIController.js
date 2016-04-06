e6b95d47f9855cc5a8ae2261c47ad698399b443a
Happy Code Labs <happycodelabs@iMac.local>
2016-04-06 12:09:58 +0530

    Second commit

---
+++ mic-sails/api/controllers/SampleAPIController.js
@@ -0,0 +1,11 @@
+/**
+ * SampleAPIController
+ *
+ * @description :: Server-side logic for managing sampleapis
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
